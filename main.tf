dd21cf39c33cc401a4b6f72544e73aa1ac67f536
Ubuntu <ubuntu@ip-172-31-8-99.us-west-2.compute.internal>
2023-01-20 01:19:05 +0000

    fix typo in sg

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_ rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type = "ingress"
   from_port = 80
   to_port = 80
@@ -44,7 +44,7 @@ resource "aws_security_group_ rule" "blog_http_in" {
   securit_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_ rule" "blog_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type = "ingress"
   from_port = 443
   to_port = 443
@@ -54,7 +54,7 @@ resource "aws_security_group_ rule" "blog_https_in" {
   securit_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_ rule" "blog_out" {
+resource "aws_security_group_rule" "blog_out" {
   type = "egress"
   from_port = 0
   to_port = 0

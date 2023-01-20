13dc43c2c724ce8a9b809b42d793c6434b4249f9
Ubuntu <ubuntu@ip-172-31-8-99.us-west-2.compute.internal>
2023-01-20 18:40:17 +0000

    new vpc - change vpc id ref

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
   
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id  = module.vpc.vpc_id
   name = "blog"
   
   ingress_rules = ["http-80-tcp", "https-443-tcp"]

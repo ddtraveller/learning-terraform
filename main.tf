b4300524dea3a8d387ee821dbdfd9c13ae3a1a69
Ubuntu <ubuntu@ip-172-31-8-99.us-west-2.compute.internal>
2023-01-20 19:19:59 +0000

    add alb - autoscaling

--- main.tf
+++ main.tf
@@ -32,17 +32,22 @@ module "vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.7.0"
+  # insert the 1 required variable here
+  name = "blog"
+  min_size = 1
+  max_size = 2
   
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_zone_identifier = module.vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+  
+  image_id           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -58,7 +63,7 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]  
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -98,3 +103,4 @@ module "alb" {
     Environment = "Dev"
   }
 }
+
